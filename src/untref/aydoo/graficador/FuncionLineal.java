15f7464d056996a40e0df0e7e8127557e195c284
Santiago Rojo <tiagox@gmail.com>
2014-04-13 23:47:12 -0300

    Código base.

---
+++ src/untref/aydoo/graficador/FuncionLineal.java
@@ -0,0 +1,15 @@
+package untref.aydoo.graficador;
+
+public class FuncionLineal {
+
+	public FuncionLineal(float a, float b) {
+
+	}
+
+	public float evaluarEn(float x) {
+
+		return 11.0f;
+
+	}
+
+}
---
+++ test/untref/aydoo/graficador/tests/FuncionLinealTests.java
@@ -0,0 +1,38 @@
+package untref.aydoo.graficador.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import untref.aydoo.graficador.FuncionLineal;
+
+public class FuncionLinealTests {
+
+    @Test
+    public void evaluarEn4DeberiaRetornar11CuandoAEs2YBEs3() {
+
+        float a = 2.0f;
+        float b = 3.0f;
+
+        float x = 4.0f;
+        float resultado = 11.0f;
+
+        FuncionLineal funcionLineal = new FuncionLineal(a, b);
+
+        Assert.assertEquals(resultado, funcionLineal.evaluarEn(x), 0.0001);
+
+    }
+
+  @Test
+  public void evaluarEn0DeberiaDevolverElMismoValorDeB() {
+
+    final float a = 2.0f;
+    final float b = 3.0f;
+
+    float x = 0.0f;
+
+    FuncionLineal funcionLineal = new FuncionLineal(a, b);
+
+    Assert.assertEquals(b, funcionLineal.evaluarEn(x));
+
+  }
+
+}
